0584db8ac4934ab0bd3a4f2f495ce2e8af7a1359
rtscherer <rtscherer@gmail.com>
2016-12-06 22:22:21 -0500

    updating wine model

--- api/wine/models/wine.js
+++ api/wine/models/wine.js
@@ -7,18 +7,28 @@ var Schema = mongoose.Schema;
 var WineSchema = new Schema({
     userId: String,
     listId: String,
+    lastUpdate: Date,
     list: [{
       name: String,
       description: String,
       type: String,
+      masterVarietal: String,
       country: String,
       region: String,
+      subRegion: String,
+      vineyard: String,
       year: String,
       vintage: String,
+      size: Number,
       image: String,
       notes: String,
       price: String,
-      lastUpdate: Date }]
+      quantity: Number,
+      peakDate: Date,
+      consumptionStart: Date,
+      consumptionEnd: Date,
+      personalScore: Number,
+      isFavorite: Boolean }]
 });
 
 module.exports = mongoose.model('Wine', WineSchema);
